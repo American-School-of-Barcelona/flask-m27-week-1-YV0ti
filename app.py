5248f9354e31e1853b00e0bd7e82f45cc77c17cb
YV0ti <deadyegor@gmail.com>
2026-02-27 13:53:57 +0000

    I finsihed the project

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask , render_template, request
 
 app = Flask(__name__)
 
-# TODO: Add your routes below this line
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
+
+#render_template renders the template into HTML that my browser understands
+#
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
